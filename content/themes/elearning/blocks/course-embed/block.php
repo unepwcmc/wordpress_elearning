bdab79976b4c2a5f82bb3701a3ecc0bed7914dbd
nym1k <sleeping.stars@gmail.com>
2021-07-22 15:58:58 +0100

    Hero settings for courses listing page in customizer, removing redundant

--- content/themes/elearning/blocks/course-embed/block.php
+++ content/themes/elearning/blocks/course-embed/block.php
@@ -9,7 +9,6 @@
   $course_url = block_field( 'course-url', false );
 ?>
 
-<p>This course is <?php the_title(); ?></p>
 <div class="course-embed">
   <iframe src="<?php echo $course_url; ?>" title="<?php the_title(); ?>"></iframe>
 </div>
